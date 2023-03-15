cefdf078a22fb1fc13483aa9840f2398962efafe
jhs512 <jangka512@gmail.com>
2023-03-15 11:53:14 +0900

    생성자 주입, 필드 주입

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -2,26 +2,30 @@ package com.ll.basic1.boundedContext.member.controller;
 
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class MemberController {
     private final MemberService memberService;
 
-    public MemberController() {
-        memberService = new MemberService();
+    // 생성자 주입
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
     }
 
     @GetMapping("/member/login")
     @ResponseBody
     public RsData login(String username, String password) {
-        if ( username == null || username.trim().length() == 0 ) {
+        if (username == null || username.trim().length() == 0) {
             return RsData.of("F-3", "username(을)를 입력해주세요.");
         }
 
-        if ( password == null || password.trim().length() == 0 ) {
+        if (password == null || password.trim().length() == 0) {
             return RsData.of("F-4", "password(을)를 입력해주세요.");
         }
 
